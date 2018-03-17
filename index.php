cf9a4d17d77f0763aade8f3130ebef7f186a0fb6
dggonzalezcom <dev@dggonzalez.com>
2018-03-17 10:33:05 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<?php
-/*! \mainpage A simple manual
+<?php 
+/**
+ * Soleil - Framework for web development 
+ * 
+ * @author David González <dev@dggonzalez.com>
+ * @version 0.1
+ * 
+ */ /*! \mainpage */
 
-Some general info.
-
-This manual is divided in the following sections:
-- \subpage intro
-- \subpage advanced "Advanced usage"
-*/
+ try
+ {
+    if ( @!include('soleil/autoloafd.php' ) ) 
+        throw new Exception('Soleil : FATAL ERROR : Autoload not found.');
+ } 
+ catch (Exception $e) 
+ {
+    echo "Exception capture: <pre>{$e->getMessage()}</pre>";
+ }
